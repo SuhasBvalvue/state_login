3d9d4051d8223eedcffada365365d932088a0abd
SuhasBvalvue <68270146+SuhasBvalvue@users.noreply.github.com>
2020-08-21 03:48:19 +0530

    Add files via upload

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,54 +1,58 @@
-
 import React, { Component } from "react";
 import { render } from "react-dom";
 
 class Message extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      password: "abc",
-      c_password: "abc",
-    };
+		this.state = {
+			password: "abc",
+			c_password: "pqr",
+		};
 
-    this.submitData = this.submitData.bind(this);
-  }
-  inputPassword = event => {
-    this.setState({ password: this.state.password });
-  };
+		this.submitData = this.submitData.bind(this);
+	}
+	inputPassword = event => {
+		this.setState({ password: this.state.password });
+	};
 
-  confirmPassword = event => {
-    this.setState({ c_password: event.target.value });
-  };
-  submitData(event) {
-    event.preventDefault();
-    const { password, c_password } = this.state;
-    const matches = password === c_password;
-    matches ? alert("MATCHED") : alert("NO MATCH");  
-  }
-  render() {
-    return (
-      <div class="container">
-        <form onSubmit={this.submitData}  id="contact" >
-          Username:
-          <input
-            type="text"
-            name="password"
-            onChange={this.inputPassword}
-          />
-          <br></br>
-          password
-          <input
-            type="password"
-            name="c_password"
-            onChange={this.confirmPassword}
-          />
-          <br></br>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
+	confirmPassword = event => {
+		this.setState({ c_password: this.state.c_password });
+	};
+	submitData(event) {
+		event.preventDefault();
+		const { password } = this.state;
+		const { c_password } = this.state;
+
+		var usr = document.loginform.password.value;
+		var psw = document.loginform.c_password.value;
+
+		const matches = usr === password && psw === c_password;
+		matches ? alert("MATCHED") : alert("NO MATCH");
+	}
+	render() {
+		return (
+			<div className="container">
+				<form name="loginform" onSubmit={this.submitData} id="contact">
+					Username:
+					<input
+						type="text"
+						name="password"
+						onChange={this.inputPassword}
+					/>
+					<br></br>
+					password
+					<input
+						type="password"
+						name="c_password"
+						onChange={this.confirmPassword}
+					/>
+					<br></br>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default Message
+export default Message;
